b28d5df4b3b44151536bc56891b8b5e43dc19414
Iksanard <iksanardika@gmail.com>
2026-01-04 20:41:59 +0700

    basic timer with countdown

--- app/src/main/java/com/mokaneko/pomoneko/ui/timer/TimerViewModel.kt
+++ app/src/main/java/com/mokaneko/pomoneko/ui/timer/TimerViewModel.kt
@@ -3,6 +3,10 @@ package com.mokaneko.pomoneko.ui.timer
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TimerViewModel : ViewModel() {
 
@@ -17,16 +21,45 @@ class TimerViewModel : ViewModel() {
     )
     val uiState: State<TimerUiState> = _uiState
 
+    private var timerJob: Job? = null
+    private var remainingSeconds = 25*60
+
+    private fun formatTime(seconds: Int): String {
+        val m = seconds / 60
+        val s = seconds % 60
+        return "%02d:%02d".format(m, s)
+    }
+
     fun onPlay() {
-        _uiState.value = _uiState.value.copy(timerState = TimerState.RUNNING)
+        if (timerJob != null) return
+
+        _uiState.value = uiState.value.copy(
+            timerState = TimerState.RUNNING
+        )
+
+        timerJob = viewModelScope.launch {
+            while (remainingSeconds > 0) {
+                delay(1000)
+                remainingSeconds--
+                _uiState.value = uiState.value.copy(
+                    timerText = formatTime(remainingSeconds)
+                )
+            }
+            onReset()
+        }
     }
     fun onPause() {
+        timerJob?.cancel()
+        timerJob = null
         _uiState.value = _uiState.value.copy(timerState = TimerState.PAUSED)
     }
     fun onReset() {
+        timerJob?.cancel()
+        timerJob = null
+        remainingSeconds = 25*60
         _uiState.value = _uiState.value.copy(
             timerState = TimerState.STOPPED,
-            timerText = "25:00"
+            timerText = formatTime(remainingSeconds)
         )
     }
 }
